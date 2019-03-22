fd83446f20584f943c28adebc777ddfbe0144661
cyber <cyber@cyber-PC>
2019-03-22 12:55:28 +0530

    Simple operation

---
+++ GitOperation/src/com/ASMDopr/ADSM.java
@@ -0,0 +1,24 @@
+package com.ASMDopr;
+
+public class ADSM {
+
+	int a=10;
+	int b=20;
+	public void add()
+	{
+		System.out.println("addition "+(a+b));
+	}
+	
+	public void sub()
+	{
+		System.out.println("substraction "+(a-b));
+	}
+
+	public static void main(String[] args)
+	{
+		ADSM ob = new ADSM();
+		ob.add();
+		ob.sub();
+	}
+
+}
